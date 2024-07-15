6e54910c994c2ea7398c41b242594e2ea1fefa36
Jessada Trirongkit <jessada.trirongkit@gmail.com>
2024-07-15 15:18:29 +0700

    add createdAt

--- src/certificate/services.js
+++ src/certificate/services.js
@@ -2,5 +2,10 @@ import { ref, set } from "firebase/database";
 import { database } from "../lib/firebase";
 
 export async function addToHallOfFame({ name, faculty, id }) {
-  await set(ref(database, `/hall-of-fame/${id}`), { name, faculty, id });
+  await set(ref(database, `/hall-of-fame/${id}`), {
+    name,
+    faculty,
+    id,
+    createdAt: new Date().toISOString(),
+  });
 }
